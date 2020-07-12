73ff18709d5f88a6ef4a4857dcb96c4d0d161062
yd27 <dhdbseh5oe@naver.com>
2020-07-12 12:08:10 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/tjoeun/lifecycle_20200712/BaseActivity.kt
@@ -0,0 +1,12 @@
+package kr.co.tjoeun.lifecycle_20200712
+
+import androidx.appcompat.app.AppCompatActivity
+
+open class BaseActivity {
+
+    abstract class BaseActivity : AppCompatActivity() {
+
+        val mContext = this
+    }
+
+    abstract class BaseActivity
